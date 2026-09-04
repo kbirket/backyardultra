da479f374ba5470aa1499effc8983686668dde1f
kbirket <kbirket@pattersonhc.org>
2026-09-04 14:26:44 -0500

    Simplify config to API URL only

--- config.js
+++ config.js
@@ -1,6 +1,2 @@
 // Vercel version: no secrets belong in this file.
 window.APP_CONFIG = { API_URL: "/api/race" };
-window.addEventListener('load',()=>{
-  const css=document.createElement('link'); css.rel='stylesheet'; css.href='styles-v3.css?v=20260904-ref8'; document.head.appendChild(css);
-  const js=document.createElement('script'); js.src='app-v5.js?v=20260904-ref9'; document.body.appendChild(js);
-});
